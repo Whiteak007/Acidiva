fab00e657975b3c4dbc4289b45cb2df9631e0464
Ankit Kumar <123821973+Whiteak007@users.noreply.github.com>
2025-08-22 12:10:29 +0530

    Adding Backend Url in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import ImageUpload from "./components/ImageUpload";
-export const backendUrl = "http://localhost:3000";
+export const backendUrl = "https://acidiva.vercel.app";
 function App() {
   const [count, setCount] = useState(0);
 
